d5f577339a9c717b32164a04759f305899187a2b
Andrius Bolsaitis <andrius.bolsaitis@gmail.com>
2014-03-02 23:34:16 +0000

    fixed messages and error reporting

--- controller/usersController.js
+++ controller/usersController.js
@@ -11,6 +11,10 @@ module.exports = {
 		var self = this
 		self.render = false
         User.add({name: 'Andrius', email : 'bolshas@gmail.com'}, function(error) {
+            if (error)
+                self.router.session.message = { type: 'error', text: 'the user could not be added' }
+            else
+                self.router.session.message = { type: 'success', text: 'the user was added successfully' }
             return callback(error, self.router.redirect('/users'))
         })
     },
@@ -19,12 +23,17 @@ module.exports = {
         var self = this
         self.render = false
         User.delete({name: 'Andrius'}, function(error) { 
+            if (error)
+                self.router.session.message = { type: 'error', text: 'the user was not deleted' }
+            else 
+                self.router.session.message = { type: 'success', text: 'the user was deleted successfully' }
             return callback(error, self.router.redirect('/users'))
         })
     },
     
-    view: function(data) {
+    view: function(data, callback) {
 		var self = this
 		self.view.test = data
+		return callback()
     }
 }
