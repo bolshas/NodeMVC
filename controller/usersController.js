8d40255de8f37378a07f96574d1282c782ac5ce4
Andrius Bolsaitis <andrius.bolsaitis@gmail.com>
2014-03-06 23:45:45 +0000

    fixed css and errors in async controller functions.

--- controller/usersController.js
+++ controller/usersController.js
@@ -9,25 +9,23 @@ module.exports = {
     
     add: function(data, callback) {
 		var self = this
-		self.render = false
         User.add({name: 'Andrius', email : 'bolshas@gmail.com'}, function(error) {
             if (error)
                 self.router.session.message = { type: 'error', text: 'the user could not be added' }
             else
                 self.router.session.message = { type: 'success', text: 'the user was added successfully' }
-            return callback(error, self.router.redirect('/users'))
+            return callback(error, function(){self.router.redirect('/users')})
         })
     },
     
     delete: function(data, callback) {
         var self = this
-        self.render = false
         User.delete({name: 'Andrius'}, function(error) { 
             if (error)
                 self.router.session.message = { type: 'error', text: 'the user was not deleted' }
             else 
                 self.router.session.message = { type: 'success', text: 'the user was deleted successfully' }
-            return callback(error, self.router.redirect('/users'))
+            return callback(error, function(){self.router.redirect('/users')})
         })
     },
     
--- server.js
+++ server.js
@@ -2,4 +2,15 @@ var App = require('./lib/app')
 var app = new App()
 app.start()
 
+var stdin = process.stdin
+
+stdin.setRawMode(true)
+stdin.resume()
+stdin.setEncoding('utf8')
+stdin.on('data', function ( key) {
+    if (key === '\u0003')
+        process.exit()
+    if (key === '\u0063')
+        process.stdout.write('\u001B[2J\u001B[0;0f')
+})
 //to start in background: node server >> log/errors.log 2>&1 &
